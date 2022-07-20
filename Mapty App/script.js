52ee8a692f327463a7f4723089a0c9e84b45c0b1
chrisfromhood <kristijan@cogentdeciphering.com>
2022-07-20 17:18:42 +0200

    Mapty

--- Mapty App/script.js
+++ Mapty App/script.js
@@ -1,6 +1,5 @@
 'use strict';
 
-
 class Workout {
   date = new Date();
   id = (Date.now() + '').slice(-10);
@@ -23,7 +22,6 @@ class Workout {
   click() {
     this.clicks++;
   }
-
 }
 
 class Running extends Workout {
@@ -80,7 +78,13 @@ class App {
   #workouts = [];
 
   constructor() {
+    // Get user's position
     this._getPosition();
+
+    // Get data from local storage
+    this._getLocalStorage();
+
+    // Attach event handlers
     form.addEventListener('submit', this._newWorkout.bind(this));
     inputType.addEventListener('change', this._toggleElevationField);
     containerWorkouts.addEventListener('click', this._moveToPopup.bind(this));
@@ -110,6 +114,10 @@ class App {
     //on koristiomo umesto event listenera nalazi se u biblioteci
     // Handling clicks on map
     this.#map.on('click', this._showForm.bind(this));
+
+    this.#workouts.forEach(work => {
+      this._renderWorkoutMarker(work);
+    });
   }
 
   _showForm(mapE) {
@@ -171,7 +179,6 @@ class App {
 
     // Add new object to workout array
     this.#workouts.push(workout);
-    console.log(workout);
 
     // Render workout on map as marker
     this._renderWorkoutMarker(workout);
@@ -179,9 +186,11 @@ class App {
     // Render workout on list
     this._renderWorkout(workout);
 
-
     // Hide form + Clear input fields
     this._hideForm(workout);
+
+    //Set local storage to all workouts
+    this._setLocalStorage();
   }
 
   _renderWorkoutMarker(workout) {
@@ -249,12 +258,12 @@ class App {
 
   _moveToPopup(e) {
     const workoutEl = e.target.closest('.workout');
-    console.log(workoutEl);
+
 
     if (!workoutEl) return;
 
     const workout = this.#workouts.find(work => work.id === workoutEl.dataset.id);
-    console.log(workoutEl);
+
 
     this.#map.setView(workout.coords, this.#mapZoomLevel, {
       animate: true,
@@ -264,7 +273,28 @@ class App {
     });
 
     //using the public interface
-    workout.click();
+    // workout.click();
+  }
+
+  _setLocalStorage() {
+    localStorage.setItem('workouts', JSON.stringify(this.#workouts));
+  }
+
+  _getLocalStorage() {
+    const data = JSON.parse(localStorage.getItem('workouts'));
+
+    if (!data) return;
+
+    this.#workouts = data;
+
+    this.#workouts.forEach(work => {
+      this._renderWorkout(work);
+    });
+  }
+
+  reset() {
+    localStorage.removeItem('workouts');
+    location.reload();
   }
 
 }
